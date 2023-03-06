a4df0e7e921a67e68f5e7af34921df56c6a6f64d
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2023-03-06 23:01:21 +0800

    Update Hook.cpp

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -7,27 +7,18 @@
 extern string DisconnectScreen(string reason);
 
 TInstanceHook(void, "?disconnectClient@ServerNetworkHandler@@QEAAXAEBVNetworkIdentifier@@W4SubClientId@@AEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@_N@Z", ServerNetworkHandler, NetworkIdentifier* a1, int a2, string a3, bool a4) {
-    if(a3 == "disconnectionScreen.disconnected"){
+    if (a3 == "disconnectionScreen.disconnected") {
 		return original(this, a1, a2 ,a3, a4);
 	}
 	int cver = *((int*)a1 + 12);
 	int sver = SharedConstants::NetworkProtocolVersion;	
 	string str = DisconnectScreen(a3);
-    if(str == ""){
+    if (str == "") {
         return original(this, a1, a2 ,a3, a4);
     }
-    else{
+    else {
         str = ReplaceStr(str, "{Client}", std::to_string(cver));
 	    str = ReplaceStr(str, "{Server}", std::to_string(sver));
 	    return original(this, a1, a2, str, a4);
     }
 }
-
-/*
-inline T& dAccess(void* ptr) {
-    return *(T*)(((uintptr_t)ptr) + off);
-}
-dAccess<int>(a1, 12)
-
-*(int*)(a1 + 12)
-*/
